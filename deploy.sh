a24378181d74f528d4cb2b12888607075285ff3f
Harry Volker <57865118+htr-volker@users.noreply.github.com>
2020-11-05 12:25:41 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,6 @@
 sudo apt-get update
 sudo apt-get install python3 python3-pip python3-venv -y
 
-# Create and source virtual environment
-python3 -m venv venv
-source venv/bin/activate
-
 # Create service script
 cat << EOF > /opt/todo-list/todo-list.service
 [Unit]
@@ -32,6 +28,10 @@ sudo mkdir $install_dir
 sudo cp -r . $install_dir
 sudo chown -R jenkins:jenkins $install_dir
  
+# Create and source virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
 # Install pip requirements
 pip3 install -r requirements.txt
 
